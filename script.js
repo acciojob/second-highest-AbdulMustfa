b29384b789113793635f5969fe6b2c268ca1e624
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-11 11:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  // If the array is empty or has only one element, or all elements are the same, return -Infinity.
+  if (arr.length < 2 || new Set(arr).size === 1) {
+    return -Infinity;
+  }
+
+  // Find the highest element in the array.
+  let highest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > highest) {
+      highest = arr[i];
+    }
+  }
+
+  // Find the second highest element in the array.
+  let secondHighest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > secondHighest && arr[i] < highest) {
+      secondHighest = arr[i];
+    }
+  }
+
+  return secondHighest;
+}
+
